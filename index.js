b6725e5f039ada358793b789d4caa97219a3a609
Hamza Mujinovic <mujinovichamza@gmail.com>
2023-12-27 00:00:20 +0100

    Add the addSquares method to the Graph class

--- index.js
+++ index.js
@@ -30,4 +30,12 @@ class Graph {
 			console.log(`${vertex} -> ${edges.join(', ')}`);
 		}
 	}
+
+	addSquares() {
+		for (let i = 0; i < 8; i++) {
+			for (let j = 0; j < 8; j++) {
+				chessboard.addVertex([i, j]);
+			}
+		}
+	}
 }
