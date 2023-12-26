c2dcff00ae2e712286c669e3cfea94a90f095106
Hamza Mujinovic <mujinovichamza@gmail.com>
2023-12-26 23:50:07 +0100

    Add the addEdge method to the Graph class

--- index.js
+++ index.js
@@ -6,4 +6,22 @@ class Graph {
 	addVertex(vertex) {
 		this.vertices.set(vertex.toString(), []);
 	}
+
+	addEdge(vertex1, vertex2) {
+		const key1 = vertex1.toString();
+		const key2 = vertex2.toString();
+
+		if (!this.vertices.has(key1) || !this.vertices.has(key2)) {
+			console.error('Invalid vertices.');
+			return;
+		}
+
+		if (!this.vertices.get(key1).includes(key2)) {
+			this.vertices.get(key1).push(key2);
+		}
+
+		if (!this.vertices.get(key2).includes(key1)) {
+			this.vertices.get(key2).push(key1);
+		}
+	}
 }
